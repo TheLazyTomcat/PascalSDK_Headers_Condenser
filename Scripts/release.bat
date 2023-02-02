1abf392d7628f992b9f1f9e055390a8c4627d85e
František Milt <frantisek.milt@gmail.com>
2023-02-02 23:29:13 +0100

    Finalized readme, add linux builds to release script

--- Scripts/release.bat
+++ Scripts/release.bat
@@ -12,5 +12,7 @@ COPY ..\readme.txt ..\Release\readme.txt
 COPY ..\license.txt ..\Release\license.txt
 
 COPY "..\Delphi\Release\win_x86\Condenser.exe" "..\Release\Condenser.exe"
+COPY "..\Lazarus\Release\lin_x86\Condenser" "..\Release\condenser_lin32"
+COPY "..\Lazarus\Release\lin_x64\Condenser" "..\Release\condenser_lin64"
 
 ENDLOCAL
